a387f169d134d74b5dd001b014700052b7f24c5b
id1508 <id1508-github@yahoo.com>
2020-01-20 15:20:22 +0100

    Fix for a Bug when 2 different locations were triggered at same time
    - Fix in LocationUtils.inside: calculation was wrong

--- app/src/test/java/ryey/easer/skills/usource/location/LocationUtilsTest.java
+++ app/src/test/java/ryey/easer/skills/usource/location/LocationUtilsTest.java
@@ -3,11 +3,23 @@ package ryey.easer.skills.usource.location;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.text.ParseException;
+
 public class LocationUtilsTest {
+    @Test
+    public void IsInsideTest() throws ParseException {
+        Assert.assertTrue(
+                LocationUtils.inside(LatLong.fromString("50, 50"),150000, LatLong.fromString("50, 51"))
+        );
+        Assert.assertFalse(
+                LocationUtils.inside(LatLong.fromString("50, 50"),100000, LatLong.fromString("50, 51"))
+        );
+    }
+
     @Test
     public void When_locationIsNull_IsAcceptable_Should_ReturnFalse(){
         Assert.assertFalse(
-            LocationUtils.isAcceptable(new LocationUSourceDataFactory().dummyData(), null)
+                LocationUtils.isAcceptable(new LocationUSourceDataFactory().dummyData(), null)
         );
     }
 }
